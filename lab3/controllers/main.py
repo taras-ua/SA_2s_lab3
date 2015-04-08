99987ebd06d80d7722834452b374101c69371718
taras <taras@devmiles.com>
2015-04-08 14:02:54 +0300

    basic io

--- lab3/controllers/main.py
+++ lab3/controllers/main.py
@@ -1,21 +1,21 @@
-import random
 import networkx as nx
 from networkx.readwrite import json_graph
 import numpy as np
+import urllib.parse
 from numpy import linalg as la
 
 
 def build_request(form):
-    request = '/graph/?'
-    request += 'data={}'.format(form.cleaned_data['input_data'])
+    request = '/graph/?data='
+    request += urllib.parse.quote_plus(form.cleaned_data['graph_matrix'].replace('\n', ' ').replace('\r', ''))
     return request
 
 
 def get_graph(request):
-    input_data = int(request.GET.get('data'))
-    graph = sample_graph()
+    input_data = request.GET.get('data')
+    matrix = np.matrix(input_data)
+    graph = nx.from_numpy_matrix(matrix)
     if graph is not None:
-        matrix = nx.to_numpy_matrix(graph)
         np.set_printoptions(suppress=True, precision=5)
         eigenvals, eigenvecs = la.eig(matrix)
         eigenstring = ''
@@ -23,30 +23,10 @@ def get_graph(request):
         eig_num = 0
         for i in range(eigenvals.size):
             val = eigenvals[i]
-            matrix_string += str(matrix[i, :]).replace('[', '').replace(']', '') + '<br>'
-            if val != 0:
-                eig_num += 1
-                eigenstring += '<b>&lambda;<sub>' + str(eig_num) + '</sub></b> = ' + str(val) + '; '
-                eigenstring += '<b>f<sub>' + str(eig_num) + '</sub></b> = ( '
-                eigenstring += str(eigenvecs[:, i]).replace('[', '').replace(']', '')
-                eigenstring += ' )<sup>T</sup><br>'
+            matrix_string += str(matrix[i, :]) + '<br>'
+            eig_num += 1
+            eigenstring += '<b>&lambda;<sub>' + str(i+1) + '</sub></b> = ' + str(val) + '<br>'
         return json_graph.node_link_data(graph), matrix_string, eigenstring
+    return None
 
 
-def sample_graph():
-    G = nx.MultiDiGraph()
-    for i in range(3):
-        for j in range(3):
-            v = i * 3 + j
-            G.add_node(v)
-            probability = random.uniform(0, 1)
-            prob_sum = 1 / (2 * G.number_of_nodes() - 1)
-            if probability < prob_sum:
-                G.add_edge(v, v)
-            else:
-                for node in range(G.number_of_nodes()):
-                    prob_sum += G.degree(node) / (2 * G.number_of_nodes() - 1)
-                    if probability < prob_sum:
-                        G.add_edge(v, node)
-                        break
-    return G
--- lab3/views.py
+++ lab3/views.py
@@ -7,16 +7,39 @@ import lab3.controllers.main as controller
 
 
 class GraphForm(forms.Form):
-    input_data = forms.IntegerField(min_value=0)
+    graph_matrix = forms.CharField(widget=forms.Textarea())
+
+
+def valid_matrix(form):
+    matrix_string = form.cleaned_data['graph_matrix'].replace('\n', ' ').replace('\r', '')
+    row_strings = matrix_string.split(';')
+    row_count = len(row_strings)
+    if row_count < 1:
+        return False
+    else:
+        for row in row_strings:
+            if len(row.split()) != row_count:
+                return False
+    return True
 
 
 def home(request):
     if request.method == 'GET':
-        form = GraphForm(initial={'input_data': 1})
+        form = GraphForm(initial={'graph_matrix':
+                                      '   0 -0.7    0    0 -0.8 -0.5    0    0    0;\n' +
+                                      '-0.5    0    0  0.1 -0.2 -0.3    0 -0.2    0;\n' +
+                                      '   0    0    0    0  0.5  0.2    0 -0.1    0;\n' +
+                                      '   0    0    0    0 -0.1    0    0 -0.1    0;\n' +
+                                      '   0    0  0.2    0    0    0    0    0    0;\n' +
+                                      ' 0.1  0.2  0.6    0  0.2    0  0.7  0.1  0.3;\n' +
+                                      '   0    0    0    0 -0.1    0    0    0  0.9;\n' +
+                                      '   0    0  0.3  0.5    0    0    0    0  0.1;\n' +
+                                      '   0    0  0.1  0.1    0    0    0    0    0'
+                                  })
         return render_to_response('home.html', {'form': form}, context_instance=RequestContext(request))
     if request.method == 'POST':
         form = GraphForm(request.POST)
-        if form.is_valid():
+        if form.is_valid() and valid_matrix(form):
             return redirect(controller.build_request(form))
         else:
             return HttpResponse(status=500)
